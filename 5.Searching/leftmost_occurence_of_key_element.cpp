910e26cf8a033d7ed39e0a19d5df00da3eace8d2
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-04-08 13:29:07 +0530

    Add files via upload

---
+++ 5.Searching/leftmost_occurence_of_key_element.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+int find_left_most_element(int arr[] , int n, int key) {
+	
+	int s = 0, e = n-1; 
+	while(s <= e) {
+	
+	int mid = s + (e-s)/2;
+	
+	if(arr[mid] > key) {
+		e = mid - 1;
+	}
+	else if(arr[mid] < key) {
+		s = mid + 1;
+	}
+	
+	else {
+		
+		if(mid == 0 || arr[mid] != arr[mid-1]) {
+			return mid;
+		}
+		
+		else {
+			e = mid - 1;
+		}
+		
+	}
+}
+
+	return -1;
+}
+
+
+
+int main() {
+	
+	int n;
+	cout<<"enter the size of array :"<<endl;
+	cin>>n;
+	int arr[n];
+	cout<<"enter the elements of the array :"<<endl;
+	for(int i = 0; i<n; i++) {
+		cin>>arr[i];
+	}
+	
+	int key;
+	cout<<"enter the key element :"<<endl;
+	cin>>key;
+	
+	cout<<"index of the key element is = "<<find_left_most_element(arr,n,key);
+	
+	
+}
